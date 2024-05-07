815a3c969da60c83189cc21d29756129067593d1
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-05-06 19:42:52 -0500

    Add files via upload

---
+++ WAP_Lab7/exe1.js
@@ -0,0 +1,64 @@
+// 1. Using Arrow Function
+let group = {
+    title: "Our Group",
+    students: ["John","Pete","Alice"],
+    showList: function(){
+        this.students.forEach(student => {
+            console.log(this.title + ":" + student);
+        });
+    }
+};
+group.showList();
+
+// 2. Using Bind
+// let group = {
+//     title: "Our Group",
+//     students: ["John","Pete","Alice"],
+//     showList: function(){
+//         this.students.forEach(function(student){
+//             console.log(this.title + ":" + student);
+//         }.bind(this));
+//     }
+// };
+// group.showList();
+
+// 3. Using Variable
+// let group = {
+//     title: "Our Group",
+//     students: ["John","Pete","Alice"],
+//     showList: function(){
+//         let self = this;
+//         this.students.forEach(function(student){
+//             console.log(self.title + ":" + student);
+//         });
+//     }
+// };
+// group.showList();
+
+// 4. Using Function Expression
+// let group = {
+//     title: "Our Group",
+//     students: ["John","Pete","Alice"],
+//     showList: function(){
+//         let self = this;
+//         const result = s => {
+//             console.log(this.title + ":" + s);
+//         }
+//         this.students.forEach(result);
+//     }
+// };
+// group.showList();
+
+//5. Using invoke function
+// let group = {
+//     title: "Our Group",
+//     students: ["John","Pete","Alice"],
+//     showList: function(){
+//         let self = this;
+//         this.students.forEach(function(student){
+//             console.log(self.title + ":" + student);
+//         },this);
+//     }
+// };
+// group.showList();
+
