b8934c2220cf17c0c91a40058c39b4cb21b99854
htetyadanar-hyo <149476965+htetyadanar-hyo@users.noreply.github.com>
2024-05-06 21:52:05 -0500

    Update exe1.js

--- WAP_Lab7/exe1.js
+++ WAP_Lab7/exe1.js
@@ -11,29 +11,29 @@ let group = {
 group.showList();
 
 // 2. Using Bind
-// let group = {
-//     title: "Our Group",
-//     students: ["John","Pete","Alice"],
-//     showList: function(){
-//         this.students.forEach(function(student){
-//             console.log(this.title + ":" + student);
-//         }.bind(this));
-//     }
-// };
-// group.showList();
+let group = {
+    title: "Our Group",
+    students: ["John","Pete","Alice"],
+    showList: function(){
+        this.students.forEach(function(student){
+            console.log(this.title + ":" + student);
+        }.bind(this));
+    }
+};
+group.showList();
 
 // 3. Using Variable
-// let group = {
-//     title: "Our Group",
-//     students: ["John","Pete","Alice"],
-//     showList: function(){
-//         let self = this;
-//         this.students.forEach(function(student){
-//             console.log(self.title + ":" + student);
-//         });
-//     }
-// };
-// group.showList();
+let group = {
+    title: "Our Group",
+    students: ["John","Pete","Alice"],
+    showList: function(){
+        let self = this;
+        this.students.forEach(function(student){
+            console.log(self.title + ":" + student);
+        });
+    }
+};
+group.showList();
 
 //4. Using apply functon
 let group = {
@@ -49,15 +49,15 @@ let group = {
 group.showList();
 
 //5. Using invoke function
-// let group = {
-//     title: "Our Group",
-//     students: ["John","Pete","Alice"],
-//     showList: function(){
-//         let self = this;
-//         this.students.forEach(function(student){
-//             console.log(self.title + ":" + student);
-//         },this);
-//     }
-// };
-// group.showList();
+let group = {
+    title: "Our Group",
+    students: ["John","Pete","Alice"],
+    showList: function(){
+        let self = this;
+        this.students.forEach(function(student){
+            console.log(self.title + ":" + student);
+        },this);
+    }
+};
+group.showList();
 
